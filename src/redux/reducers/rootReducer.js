3c61e979f28e3e9aea9e989f68334d7408ba147b
Peter <petersolomon704@gmail.com>
2023-04-27 05:53:05 +0100

    Anecdote
    Added anecdote reducer

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import counterReducer from "./counter/counter";
+import { anecdoteReducer } from "./anecdote/anecdoteReducer";
 
 const rootReducers = combineReducers({
   counter: counterReducer,
+  anecdotes: anecdoteReducer,
 });
 
 export default rootReducers;
