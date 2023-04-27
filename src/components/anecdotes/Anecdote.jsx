8898a92f0eb8d783a173f46403e5b609d3c3fc1a
Peter <petersolomon704@gmail.com>
2023-04-27 06:38:47 +0100

    anecdote
    Modified code

--- src/components/anecdotes/Anecdote.jsx
+++ src/components/anecdotes/Anecdote.jsx
@@ -10,11 +10,11 @@ const Anecdote = () => {
     <div className="container">
       <h2>Anecdotes</h2>
       {anecdotes.map((anecdote) => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
+        <div key={anecdote.id} className="anecdote-wrapper">
+          <div className="content">{anecdote.content}</div>
           <div className="vote-counts">
             has {anecdote.votes}
-            <button>vote</button>
+            <button className="vote-btn">vote</button>
             {/* <button onClick={() => vote(anecdote.id)}>vote</button> */}
           </div>
         </div>
